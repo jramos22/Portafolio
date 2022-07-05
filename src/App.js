f2eb37e9dacabc1a939614fd9e68a320b4fd0770
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2022-07-05 14:53:06 -0600

    arreglo el reactRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './sass/app.scss';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import {
-  BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
@@ -11,25 +10,24 @@ import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Projects from './pages/Projects';
 import Header from './components/Header';
-import { MobileFooter} from './components/Footer';
+import { MobileFooter } from './components/Footer';
 
 function App() {
   useEffect(() => {
-    AOS.init({duration:1000, once:true});
+    AOS.init({ duration: 1000, once: true });
     AOS.refresh();
   }, []);
   return (
-      <Router>
-          <Routes>
-            <Header/>
-            <Route path='/about' element={<AboutMe />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/' element={<Home />} />
-          </Routes>
-        <MobileFooter/>
-          
-      </Router>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/about' element={<AboutMe />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <MobileFooter />
+    </>
   );
 }
 
